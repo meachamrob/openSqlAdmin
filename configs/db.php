23262577faace0da34ad99ed44d37718a66c0eae
Thierry <thierry@quiksilver.fr>
2011-04-22 16:14:54 +0200

    Gpl terms

--- configs/db.php
+++ configs/db.php
@@ -1,12 +1,29 @@
 <?php
 
-# Connection mySQL :
+    /*
+    This file is part of openSqlAdmin.
 
-$db = array(
-    'HOST'	=> '',
-    'DATABASE'	=> '',
-    'USER'	=> '',
-    'PASSWORD'	=> ''
-);
+    openSqlAdmin is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    openSqlAdmin is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+    */
+    
+    # Connection mySQL :
+
+    $db = array(
+        'HOST'	=> '',
+        'DATABASE'	=> '',
+        'USER'	=> '',
+        'PASSWORD'	=> ''
+    );
 
 ?>
